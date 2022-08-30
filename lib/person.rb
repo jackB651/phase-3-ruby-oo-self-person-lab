84f98a0bf16a8883f4ad6db2e9aabce168d361c8
Jonathon <jwbialas@gmail.com>
2022-08-30 16:06:33 -0500

    commit tree 60f00e6120f6c115170f2f119670220b52e34f77

--- lib/person.rb
+++ lib/person.rb
@@ -19,6 +19,7 @@ class Person
            @happiness = happiness 
         end
     end
+
     def hygiene= (hygiene)
         if hygiene > 10
             @hygiene = 10
@@ -28,6 +29,7 @@ class Person
            @hygiene = hygiene 
         end
     end
+
     def happy?
         if happiness > 7
             true
@@ -35,6 +37,7 @@ class Person
             false
       end
     end
+
     def clean?
         if hygiene > 7
             true
@@ -42,18 +45,43 @@ class Person
             false
       end
     end
+
     def get_paid (amount)
        @bank_account = bank_account + amount
         "all about the benjamins"
     end
-    def take_bath
-        @hygiene = @hygiene + 4
 
-        if @hygiene > 10
-            @hygiene  = 10    
-      else
-           @hygiene = @hygiene       
-      end
+    def take_bath
+        hygiene = @hygiene + 4
+        self.hygiene= hygiene
         "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        hygiene = @hygiene - 3
+        self.hygiene= hygiene
+        happiness = @happiness + 2
+        self.happiness= happiness
+         "♪ another one bites the dust ♫"
+    end
+
+    def call_friend (friend)
+        self.happiness= @happiness + 3
+        friend.happiness= (friend.happiness + 3)
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness= @happiness - 2
+            person.happiness= (person.happiness - 2)
+           'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
+            self.happiness= @happiness + 1
+            person.happiness= (person.happiness + 1)
+            'blah blah sun blah rain' 
+        else 
+           'blah blah blah blah blah'
+        end 
     end
 end
